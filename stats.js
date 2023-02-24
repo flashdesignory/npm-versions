3c6100b847598ac0b8fc50e2075078bc84711cb6
Thorsten Kober <tkober@gmail.com>
2023-02-24 13:43:11 -0800

    initial commit

---
+++ stats.js
@@ -0,0 +1,42 @@
+/**
+ * Instructions
+ * go to: https://www.npmjs.com/package/react?activeTab=versions
+ * open console and copy window.__context__ object
+ * save to json file
+ * replace require import to point to new file
+ */
+
+const fs = require("fs").promises;
+
+const parse = async (file) => {
+    const data = require(file);
+    const { context } = data;
+    const { versionsDownloads } = context;
+
+    const versions = Object.entries(versionsDownloads).map((entry) => {
+        return { version: entry[0], downloads: entry[1] };
+    });
+
+    const descending = (a, b) => b.downloads - a.downloads;
+    versions.sort(descending);
+    const results = versions.slice(0, 6);
+    const { package: name } = context;
+    const stats = await fs.stat(file);
+    const mtime = new Date(stats.mtime).toLocaleDateString();
+    const title = `${name} versions - last 7 days (${mtime})`;
+    console.log(title);
+    console.log(results);
+
+    // read html file
+    let html = await fs.readFile("index.html", "utf8");
+    html = html.replace(
+        /const data = \[.*?\]/,
+        `const data = ${JSON.stringify(results)}`
+    );
+    html = html.replace(/const title = ".*?"/, `const title = "${title}"`);
+    // write html files
+    await fs.writeFile("index.html", html);
+    console.log("done");
+};
+
+parse("./example.json");
